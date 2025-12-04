ed8ad43e805c45c9524015f8bdd21fd22b9b25a5
suraj rawat <surajrwat001@gmail.com>
2025-12-04 10:20:00 +0530

    Added task-3 folder

---
+++ task-3/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import '../src/App.css'
+
+const PasswordToggle = () => {
+
+  const [show, setShow] = useState(false);
+  const [pw, setPw] = useState("");
+
+  return (
+    <div>
+      <input type={show ? "text" : "password"}
+        value={pw}
+        placeholder='Enter Password'
+        onChange={(e) => setPw(e.target.value)} />
+
+      <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
+      <p>Length: {pw.length}</p>
+    </div>
+  )
+}
+
+export default PasswordToggle
